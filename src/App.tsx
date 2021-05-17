5df23444753eb85d68b13c6c19d63acf8e19a6e2
Mikhail Goloto <goloto@pm.me>
2021-05-17 18:10:35 +0300

    add styled components lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
+  const Header = styled.header`
+    height: 10vh;
+    background-color: cornflowerblue;
+  `;
+
+  const Footer = styled.footer`
+    height: 10vh;
+    background-color: grey;
+  `;
+
+  const Main = styled.main`
+    min-height: 80vh;
+  `;
+
   return (
     <div className="App">
-      <header></header>
-      <footer></footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
